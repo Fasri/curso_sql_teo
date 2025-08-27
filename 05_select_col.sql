08401577ad682f1c7d07a4f44c340088283b3e45
Fasri <felipeasr@gmail.com>
2025-08-27 09:30:06 -0300

    substrig

--- 05_select_col.sql
+++ 05_select_col.sql
@@ -1,4 +1,6 @@
 SELECT IdCliente,
-        QtdePontos,
-        QtdePontos + 10 AS PontosFinais
+        --QtdePontos,
+        --QtdePontos + 10 AS PontosFinais
+        DtCriacao,
+        substr(DtCriacao, 1,10) AS AnoCriacao
 FROM   Clientes;
