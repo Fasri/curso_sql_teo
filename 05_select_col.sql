a0b6b8047a7d0c5fccde0436cd8d74a3bf12cacb
Fasri <felipeasr@gmail.com>
2025-08-27 09:39:28 -0300

    datetime dias da semana

--- 05_select_col.sql
+++ 05_select_col.sql
@@ -2,5 +2,6 @@ SELECT IdCliente,
         --QtdePontos,
         --QtdePontos + 10 AS PontosFinais
         DtCriacao,
-        substr(DtCriacao, 1,10) AS AnoCriacao
+        datetime(substr(DtCriacao, 1,10)) AS AnoCriacao,
+        strftime('%w', datetime(substr(DtCriacao, 1,10))) AS DiaSemana
 FROM   Clientes;
